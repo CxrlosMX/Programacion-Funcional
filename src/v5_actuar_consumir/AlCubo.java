7eb9983201b006b179c34e971b2cf74b932593c9
CxrlosMX <LuisCRendon131@gmail.com>
2021-08-19 00:00:50 -0500

    v5_actuar y consumir

---
+++ src/v5_actuar_consumir/AlCubo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package v5_actuar_consumir;
+
+
+
+/**
+ *
+ * @author: CxrlosMX
+ * @Git-Hub: https://github.com/CxrlosMX
+ * @Phone: 953-212-97-27
+ * @Email: LuisCRendon131@gmail.com
+ * @Date: 17/08/2021
+ * 
+ */
+public class AlCubo implements Funcion{
+
+    @Override
+    public Integer transformar(Integer valor) {
+    return (int)(Math.pow(valor,3));
+    }
+
+}
---
+++ src/v5_actuar_consumir/Proveedor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package v5_actuar_consumir;
+
+
+
+import java.util.List;
+
+/**
+ *
+ * @author CxrlosMX
+ */
+public interface Proveedor {
+    
+   Integer obtener();
+    
+    
+    
+}
